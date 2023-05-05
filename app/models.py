22856285f2cbbc0bd87d303e77e0d96eaef84762
sibylassana95 <sibyamara95@gmail.com>
2023-05-05 15:57:46 +0000

    update data

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Traduction(models.Model):
-    francais = models.CharField(max_length=255)
+    francais = models.CharField(max_length=255,db_index=True)
     soninke = models.CharField(max_length=255)
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -1,42 +1,53 @@
 import json
 import requests
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Traduction
 
-from django.http import JsonResponse
+# Charger les données de traduction en mémoire à l'initialisation de l'application
+url = "https://raw.githubusercontent.com/sibylassana95/Fran-ais-Soninke/main/data/langue.json"
+response = requests.get(url)
+data = json.loads(response.text)
+traductions = {}
+for traduction in data:
+    francais = list(traduction.keys())[0].capitalize()
+    soninke = list(traduction.values())[0]
+    traductions[francais] = soninke
 
 def suggestions(request):
     phrase_francaise = request.GET.get('phrase', '')
     suggestions = []
     if phrase_francaise:
-        suggestions = Traduction.objects.filter(Q(francais__icontains=phrase_francaise) | Q(francais__istartswith=phrase_francaise)).values_list('francais', flat=True)[:10]
-    return JsonResponse(list(suggestions), safe=False)
+        # Utiliser la méthode "values" pour ne récupérer que la colonne "francais"
+        suggestions_qs = Traduction.objects.filter(
+            Q(francais__icontains=phrase_francaise) | Q(francais__istartswith=phrase_francaise)
+        ).values('francais')[:5]
+        suggestions = [s['francais'] for s in suggestions_qs]
+    return JsonResponse(suggestions, safe=False)
+
+
 
 
 def traduction(request):
-    url = "https://raw.githubusercontent.com/sibylassana95/Fran-ais-Soninke/main/data/langue.json"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    for traduction in data:
-        francais = list(traduction.keys())[0]
-        soninke = list(traduction.values())[0]
-        francais = francais.capitalize()
-        if not Traduction.objects.filter(francais=francais).exists():
-            Traduction.objects.create(francais=francais, soninke=soninke)
-
     phrase_francaise = request.POST.get('phrase_francaise', '').strip().capitalize()
     suggestions = []
     if phrase_francaise:
-        suggestions = Traduction.objects.filter(Q(francais__icontains=phrase_francaise) | Q(francais__istartswith=phrase_francaise)).values_list('francais', flat=True)[:5]
+        # Utiliser la méthode "values" pour ne récupérer que la colonne "francais"
+        suggestions_qs = Traduction.objects.filter(
+            Q(francais__icontains=phrase_francaise) | Q(francais__istartswith=phrase_francaise)
+        ).values('francais')[:5]
+        suggestions = [s['francais'] for s in suggestions_qs]
 
     traduction_soninke = ''
-    if request.method == 'POST':
-        try:
-            traduction_soninke = Traduction.objects.get(francais=phrase_francaise).soninke
-        except Traduction.DoesNotExist:
+    if phrase_francaise:
+        if phrase_francaise in traductions:
+            traduction_soninke = traductions[phrase_francaise]
+        else:
             traduction_soninke = "Traduction indisponible pour l'instant"
     return render(request, 'index.html', {'traduction_soninke': traduction_soninke, 'suggestions': suggestions, 'phrase_francaise': phrase_francaise})
+
+
 
 
 def about(request):
