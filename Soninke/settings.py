46d33eb84dbccb3d07466cd57ff4073e2edd6675
sibylassana95 <sibyamara95@gmail.com>
2024-08-25 13:23:13 +0000

    update project

--- Soninke/settings.py
+++ Soninke/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'Soninke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -116,15 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+MEDIA_URL = 'images/'
+MEDIA_ROOT = 'images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
---
+++ build_files.sh
@@ -0,0 +1,20 @@
+echo "BUILD START"
+# Clear pip cache
+pip cache purge
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.12 -m venv venv
+# activate the virtual environment
+source venv/bin/activate
+pip install --upgrade setuptools
+
+# install all deps in the venv
+pip install psycopg2-binary
+pip install -r requirements.txt
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
+python3.12 manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
