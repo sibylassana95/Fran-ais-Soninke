9c5507080e7ec465c1ba9e838bb48c8329fbbf29
sibylassana95 <sibyamara95@gmail.com>
2024-08-25 17:20:29 +0000

    update project

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 # Run Django management commands
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
