f9c4dea9760134bd5f86674c60dc40c7eb8a0ef6
sibylassana95 <sibyamara95@gmail.com>
2024-08-25 17:36:37 +0000

    update project

--- Soninke/settings.py
+++ Soninke/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
