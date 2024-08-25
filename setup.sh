e9bc0adc75bf0300bdf6ac9023f0ae8a40beda8f
sibylassana95 <sibyamara95@gmail.com>
2024-08-25 18:05:03 +0000

    update project

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
-pip cache purge
+echo "BUILD START"
 
-# Install dependencies
-pip install setuptools
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+pip install --upgrade setuptools
+# install all deps in the venv
 pip install -r requirements.txt
+pip install django-cors-headers
+# collect static files using the Python interpreter from venv
+python3.9 manage.py collectstatic 
 
-# Run Django management commands
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
