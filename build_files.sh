230efaa59680837d3c6d406a4d4848eb97186669
sibylassana95 <sibyamara95@gmail.com>
2024-08-25 13:50:05 +0000

    update project

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,10 @@
-echo "BUILD START"
-# Clear pip cache
 pip cache purge
 
-pip install --upgrade setuptools
-
-# install all deps in the venv
+# Install dependencies
+pip install setuptools
 pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
 
-echo "BUILD END"
-
-# [optional] Start the application here 
-# python manage.py runserver
+# Run Django management commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
