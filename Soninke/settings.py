f388f74548a8f5b6302ff86b357c461090cdaa6f
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2024-08-30 12:10:27 +0000

    Update settings.py

--- Soninke/settings.py
+++ Soninke/settings.py
@@ -16,7 +16,7 @@ load_dotenv(find_dotenv())
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
