e9405ea14b6cfc7342d7a96c6497d8c77eb36ffe
sibylassana95 <sibyamara95@gmail.com>
2025-01-05 22:34:24 +0000

    Ajout de django-appconf, django-compressor et rjsmin dans requirements.txt, configuration de la compression des fichiers statiques dans settings.py, et mise à jour de base.html pour utiliser la compression CSS.

--- Soninke/settings.py
+++ Soninke/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,11 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
