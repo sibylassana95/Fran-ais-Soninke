8ce15148ebc09fe9eb981f194fcc69bef1f337e0
sibylassana95 <sibyamara95@gmail.com>
2025-01-05 21:39:14 +0000

    Changement du moteur de base de données de MySQL à PostgreSQL

--- Soninke/settings.py
+++ Soninke/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Soninke.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
