ec43db21caffb860d67274f30763b2217181e11d
sibylassana95 <sibyamara95@gmail.com>
2026-03-04 22:04:55 +0000

    ajouter passenger dans gitignore et ajout des page personaliser

--- Soninke/urls.py
+++ Soninke/urls.py
@@ -3,12 +3,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
-handler404="app.views.error_404"
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static (settings. STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Error handlers
+handler400 = 'app.views.handler400'
+handler403 = 'app.views.handler403'
+handler404 = 'app.views.handler404'
+handler500 = 'app.views.handler500'
--- app/views.py
+++ app/views.py
@@ -94,6 +94,18 @@ def contribution(request):
 def about(request):
     return render(request, 'about.html')
 
-    
-def error_404(request, exception):
-    return render(request, 'page404.html')
+# Error handlers
+def handler400(request, exception):
+    return render(request, '400.html', status=400)
+
+def handler403(request, exception):
+    return render(request, '403.html', status=403)
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+def handler500(request):
+    return render(request, '500.html', status=500)
+
+def handler503(request):
+    return render(request, '503.html', status=503)
